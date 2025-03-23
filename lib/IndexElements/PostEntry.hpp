d451403734a43ba9eae51d69edc3578d96e172fb
rickey-dong <rickeyd2004@gmail.com>
2025-03-23 01:32:00 -0400

    create PostEntry class

---
+++ lib/IndexElements/PostEntry.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <stdint.h>
+#include <iostream>
+
+#include "Types.hpp"
+
+using WordOccurrences = std::vector<PostEntry>;
+
+/** 
+ * @class PostEntry
+ * @brief Represents a single occurrence of a word in the corpus.
+ *
+ * This class is the most basic, atomic part of the inverted word index.
+ * It provides capabilities to serialize and deserialize to/from storage.
+ */
+class PostEntry
+{
+    public:
+        /** 
+         * @brief Default constructor for PostEntry.
+         */
+        PostEntry();
+
+        /** 
+         * @brief Constructs a PostEntry object.
+         * @param delta The relative position of this word in the index chunk.
+         * @param location_found The section of the page where the word was found.
+         */
+        PostEntry(uint32_t delta, wordlocation_t location_found);
+
+        /**
+         * @brief Overloads the output stream operator for printing a PostEntry object.
+         * @param os The output stream.
+         * @param obj The PostEntry object to be printed.
+         * @return The modified output stream.
+         */
+        friend std::ostream& operator<<(std::ostream& os, const PostEntry& obj);
+
+        /**
+         * @brief Serializes a PostEntry object into a specific region of memory.
+         *
+         * @param base_region A pointer to the beginning of the contiguous memory region 
+         *                    where serialization will occur. This is determined by the
+         *                    uppermost parent that wants serialization. This contiguous
+         *                    memory region is memory-mapped to disk.
+         * @param offset A reference to an offset value. After serialization, 
+         *               this will be updated to point to the next available memory region.
+         * @param word_occurrence The PostEntry object to serialize.
+         *
+         * @pre `base_region` must be a valid pointer to a memory region that is mmap'ed.
+         * @pre `offset` must be a valid number such that `base_region + offset`
+         *       points to the target serialization location.
+         * 
+         * @post Writes the bytes of the PostEntry object into memory at the calculated region.
+         * @post Updates `offset` to the next available memory location.
+         */
+        static void Serialize(char* base_region, size_t &offset, const PostEntry &word_occurrence);
+
+        /**
+         * @brief Deserializes a PostEntry object from a specific region of memory.
+         *
+         * @param base_region A pointer to the beginning of the memory region containing the object.
+         *                    This memory region is memory-mapped to disk.
+         * @param offset A reference to an offset value. After deserialization, 
+         *               this will be updated to point to the next available memory region.
+         * @return The deserialized PostEntry object.
+         *
+         * @pre `base_region + offset` must point to a valid serialized PostEntry object.
+         * @post Constructs a PostEntry object from the memory bytes and returns it.
+         */
+        static PostEntry Deserialize(char* base_region, size_t &offset);
+
+    private:
+        /// The relative position of this word in the index chunk.
+        /// @todo Convert this to use relative deltas.
+        uint32_t delta;
+        
+        /// The section of the page where the word was found.
+        wordlocation_t location_found;
+};
