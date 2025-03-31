9de985a03ab4d7681d41d342851e6721addb7b17
rickey-dong <rickeyd2004@gmail.com>
2025-03-31 18:22:26 -0400

    ISRWord

--- lib/IndexStreamReaders/ISR.hpp
+++ lib/IndexStreamReaders/ISR.hpp
@@ -13,31 +13,33 @@ class ISR {
    public:
     /**
          * @brief Returns the next PostEntry that matches the occurrence of
-         * this term that this ISR is looking for.
+         * this term that this ISR is looking for and advances the ISR forward.
          * 
-         * @return A PostEntry object.
+         * @return A pointer to a PostEntry object.
          */
-    virtual PostEntry Next() = 0;
+    virtual PostEntry* Next() = 0;
 
     /**
          * @brief Returns the next PostEntry that belongs in a brand new document
-         *        that matches the occurrence of this term that this ISR is looking for.
+         *        that matches the occurrence of this term that this ISR is looking for
+         *        and advances the ISR forward.
          * 
-         * @return A PostEntry object.
+         * @return A pointer to a PostEntry object.
          */
-    virtual PostEntry NextDocument() = 0;
+    virtual PostEntry* NextDocument() = 0;
 
     /**
          * @brief Returns the first PostEntry that matches the occurrence of
-         *        this term that this ISR is looking for located at location >= target.
+         *        this term that this ISR is looking for located at location >= target
+         *        and advances the ISR forward.
          * 
          * @param target The location the ISR will start at to find the first
          *               matching PostEntry.
          * 
-         * @return A PostEntry object.
+         * @return A pointer to a PostEntry object.
          * @todo What if can't find anything?
          */
-    virtual PostEntry Seek(size_t target) = 0;
+    virtual PostEntry* Seek(size_t target) = 0;
 
     /**
          * @brief Returns the starting location of whatever the ISR is currently on.
