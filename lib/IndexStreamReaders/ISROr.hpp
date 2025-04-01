c0e3f25759cca433ab7ee34e97181679bbcf24df
rickey-dong <rickeyd2004@gmail.com>
2025-04-01 01:04:14 -0400

    fix OOP stuff

--- lib/IndexStreamReaders/ISROr.hpp
+++ lib/IndexStreamReaders/ISROr.hpp
@@ -14,55 +14,55 @@
 class ISROr : public ISR {
    public:
     /**
-         * @brief Constructs an ISROr for a group of child ISRs.
-         *
-         * @param children A vector of the children ISRs.
-         */
+     * @brief Constructs an ISROr for a group of child ISRs.
+     *
+     * @param children A vector of the children ISRs.
+     */
     ISROr(std::vector<ISR*> children);
 
     /**
-         * @brief Returns the absolute location of the earliest occurrence
-         *        among the child ISRs at this moment.
-         * 
-         * @return size_t
-         */
-    size_t GetStartLocation();
+     * @brief Returns the absolute location of the earliest occurrence
+     *        among the child ISRs at this moment.
+     * 
+     * @return size_t
+     */
+    virtual size_t GetStartLocation() override;
 
     /**
-         * @brief Returns the absolute location of the latest occurrence
-         *        among the child ISRs at this moment.
-         * 
-         * @return size_t
-         */
-    size_t GetEndLocation();
+     * @brief Returns the absolute location of the latest occurrence
+     *        among the child ISRs at this moment.
+     * 
+     * @return size_t
+     */
+    virtual size_t GetEndLocation() override;
 
     /**
-         * @brief Returns the next PostEntry that matches the occurrence of
-         * this word that this ISR is looking for and advances the ISR forward.
-         * 
-         * @return A pointer to a PostEntry object or nullptr if there is none.
-         */
+     * @brief Returns the next PostEntry that matches the occurrence of
+     * this word that this ISR is looking for and advances the ISR forward.
+     * 
+     * @return A pointer to a PostEntry object or nullptr if there is none.
+     */
     virtual PostEntry* Next() override;
 
     /**
-         * @brief Returns the next PostEntry that belongs in a brand new document
-         *        that matches the occurrence of this term that this ISR is looking for
-         *        and advances the ISR forward.
-         * 
-         * @return A pointer to a PostEntry object or nullptr if there is none.
-         */
+     * @brief Returns the next PostEntry that belongs in a brand new document
+     *        that matches the occurrence of this term that this ISR is looking for
+     *        and advances the ISR forward.
+     * 
+     * @return A pointer to a PostEntry object or nullptr if there is none.
+     */
     virtual PostEntry* NextDocument() override;
 
     /**
-         * @brief Returns the first PostEntry that matches the occurrence of
-         *        this term that this ISR is looking for located at location >= target
-         *        and advances the ISR forward.
-         * 
-         * @param target The location the ISR will start at to find the first
-         *               matching PostEntry.
-         * 
-         * @return A pointer to a PostEntry object or nullptr if there is none.
-         */
+     * @brief Returns the first PostEntry that matches the occurrence of
+     *        this term that this ISR is looking for located at location >= target
+     *        and advances the ISR forward.
+     * 
+     * @param target The location the ISR will start at to find the first
+     *               matching PostEntry.
+     * 
+     * @return A pointer to a PostEntry object or nullptr if there is none.
+     */
     virtual PostEntry* Seek(size_t target) override;
 
    private:
